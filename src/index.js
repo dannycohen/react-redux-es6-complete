2124db9872a0f85bcaa49b20086536d042d550bf
dannycohen <danny.cohen@outlook.com>
2018-01-02 11:21:28 +0200

    EOM Async in Redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import 'babel-polyfill';
 import React from 'react';
 import {render} from 'react-dom';
+import configureStore from './store/ConfigureStore';
+import {Provider} from 'react-redux';
 import {Router, browserHistory} from 'react-router';
 import routes from './routes';
+import {loadCourses} from './actions/CourseActions';
 import './styles/styles.css'; // Webpack can import CSS files too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+const store = configureStore();
+store.dispatch(loadCourses());
+
 render(
-  <Router history={browserHistory} routes={routes}/>,
-  document.getElementById('app')
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes}/>
+  </Provider>,
+  document.getElementById('app')  
 );
